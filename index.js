fd49f825dba5ae8e31cc6e4b69d15f4f2ec8869f
aida gh <aidaghaffarvand@gmail.com>
2021-09-08 15:12:44 +0430

    feat:completed task

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-
-// const button = document.querySelector("#button");
-// button.addEventListener("click", function () {
-//   const newItem = document.createElement("li");
-//   newItem.classList = "item";
-//   const inputValue = document.querySelector("#stuff").value;
-//   // Checking for input value to not be empty
-//   if (inputValue !== "") {
-//     // PUSHING THE INPUT TO A NEW TEXTNODE WE HAVE CREATED!
-//     const textnode = document.createTextNode(`${inputValue}`);
-//     newItem.appendChild(textnode);
-    
-//     const list = document.querySelector("#listItems");
-//     list.insertBefore(newItem, list.childNodes[null]);
-//     document.querySelector("#stuff").value = "";
-//   }
-// });
-
-
 const button = document.querySelector("#button");
-const todos = [];
-button.addEventListener("click", function () {
-  const inputValue = document.querySelector("#stuff").value;
-  if (inputValue !== "") {
-  todos.push(inputValue);
-  const list = document.querySelector("#listItems");
-  list.innerHTML = ''; //Reset the list content whenever we print the todos, so we don't end up with duplicate todo items.
+function remove(elemet) {
+  elemet.parentNode.removeChild(elemet);
+}
 
-    for (let i = 0; i < todos.length; i++) {
-        const li = document.createElement('li');
-        const listItem = li.appendChild(document.createTextNode(todos[i]));
-        list.appendChild(listItem);
-    }
+button.addEventListener("click", function () {
+  const checkmark = document.createElement("img");
+  checkmark.src = "checkMark.svg";
+  checkmark.id = "check";
+  const newItem = document.createElement("li");
+  newItem.classList = "item";
+  const trash = document.createElement("img");
+  trash.src = "trash.svg";
+  trash.id = "trash";
+  const inputValue = document.querySelector("#stuff").value;
+  // Checking for input value to not be empty
+  if (inputValue !== "") {
+    // PUSHING THE INPUT TO A NEW TEXTNODE WE HAVE CREATED!
+    const textnode = document.createTextNode(`${inputValue}`);
+    newItem.appendChild(checkmark);
+    newItem.appendChild(textnode);
+    newItem.appendChild(trash);
+
+    const list = document.querySelector("#listItems");
+    list.insertBefore(newItem, list.childNodes[null]);
     document.querySelector("#stuff").value = "";
+    trash.addEventListener("click", function () {
+      remove(newItem);
+    });
   }
 });
